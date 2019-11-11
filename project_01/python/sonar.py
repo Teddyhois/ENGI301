ba741f62ebf6ad498ec5913baf40dcb859591f93
teddyhois <teddyhoisington@gmail.com>
2019-11-10 23:38:08 -0600

    After MUCH troubleshooting, created python code to measure distance with both ultrasonic sensors. Note: Currently having issue with sensors cutting off and not returning values.

---
+++ project_01/python/sonar.py
@@ -0,0 +1,61 @@
+"""
+--------------------------------------------------------------------------
+Guest Counter
+--------------------------------------------------------------------------
+License:   
+Copyright 2019 Teddy Hoisington
+
+Redistribution and use in source and binary forms, with or without 
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this 
+list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, 
+this list of conditions and the following disclaimer in the documentation 
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors 
+may be used to endorse or promote products derived from this software without 
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+--------------------------------------------------------------------------
+
+Use the HT16K33 Display and a button to create a digital people counter
+
+Requirements:
+  - Increment the counter by one each time the button is pressed
+  - If button is held for more than 5s, reset the counter
+
+"""
+
+
+import time
+import board
+from adafruit_hcsr04 import HCSR04
+
+
+sonar0 = HCSR04(board.P2_2, board.P2_4)
+sonar1 = HCSR04(board.P2_6, board.P2_8)
+
+try:
+    while True:
+        try:
+            print("0:",sonar0.distance)
+            
+            print("1:",sonar1.distance)
+        except RuntimeError:
+            pass
+except KeyboardInterrupt:
+    pass
+
